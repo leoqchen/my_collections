60d996e43507ec8141a417ce5280c7abbb94452f
cqq <leo.q.chen@hotmail.com>
2024-01-14 16:19:27 +0800

    initial commit

---
+++ clone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p gl
+cd gl
+git clone https://github.com/JoeyDeVries/LearnOpenGL.git
+git clone git://anongit.freedesktop.org/git/mesa/demos mesa_demo
+git clone https://github.com/danginsburg/opengles3-book.git
+git clone https://github.com/g-truc/ogl-samples.git
+git clone https://github.com/orcaman/openglbk.git openglredbook
+git clone https://github.com/capnramses/antons_opengl_tutorials_book.git
+git clone https://github.com/KhronosGroup/VK-GL-CTS.git
