393e13f200fa6753c7b6d0f593b02966bb8ae7b5
cqq <leo.q.chen@hotmail.com>
2024-01-15 15:13:54 +0800

    openglrb4 repos link change

--- clone.sh
+++ clone.sh
@@ -6,7 +6,7 @@ git clone https://github.com/JoeyDeVries/LearnOpenGL.git
 git clone git://anongit.freedesktop.org/git/mesa/demos mesa_demo
 git clone https://github.com/danginsburg/opengles3-book.git
 git clone https://github.com/g-truc/ogl-samples.git
-git clone https://github.com/orcaman/openglbk.git openglrb4
+git clone https://github.com/leoqchen/openglrb4.git
 git clone https://github.com/openglsuperbible/sb7code.git openglsb7
 git clone https://github.com/capnramses/antons_opengl_tutorials_book.git
 git clone https://github.com/glfw/glfw.git
