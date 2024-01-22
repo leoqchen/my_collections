a0b7e07b9c6c8961cfa1584d0f4dee0e91fcc18f
cqq <leo.q.chen@hotmail.com>
2024-01-22 14:29:52 +0800

    add freeglut repos

--- clone.sh
+++ clone.sh
@@ -10,6 +10,7 @@ git clone https://github.com/leoqchen/openglrb4.git
 git clone https://github.com/openglsuperbible/sb7code.git openglsb7
 git clone https://github.com/capnramses/antons_opengl_tutorials_book.git
 git clone https://github.com/glfw/glfw.git
+git clone https://github.com/freeglut/freeglut.git
 git clone https://github.com/KhronosGroup/VK-GL-CTS.git
 
 wget http://openglsuperbible.com/files/superbible7-media.zip && unzip superbible7-media.zip -d openglsb7/bin/media/
