82ae3cb109a85172330ea73ec75551bb4de5ee17
cqq <leo.q.chen@hotmail.com>
2024-08-25 17:06:42 +0800

    minor update

--- ubuntu/apt.sh
+++ ubuntu/apt.sh
@@ -17,6 +17,8 @@
 #   LunarG Vulkan SDK
 #     https://vulkan.lunarg.com/sdk/home
 
+source /etc/os-release
+
 packages="\
   synaptic
   git gitk
@@ -28,7 +30,7 @@ packages="\
   gparted
   nautilus-dropbox
   unrar
-  gnome-shell-extensions gnome-shell-extension-manager
+  gnome-shell-extensions
   virtualbox virtualbox-ext-pack virtualbox-guest-additions-iso virtualbox-guest-x11
   manpages manpages-dev manpages-posix manpages-posix-dev
   make cmake cmake-qt-gui meson ninja-build autoconf automake bear
@@ -41,10 +43,26 @@ packages="\
   libdrm-dev libunwind-dev zlib1g-dev libelf-dev libncurses-dev libssl-dev xutils-dev libglib2.0-dev libltdl-dev
   mesa-utils mesa-common-dev libopengl-dev libgl1-mesa-dev libgles-dev libgles1 libgles2-mesa-dev libegl-dev libegl1-mesa-dev libglu1-mesa libgdm-dev libglvnd-core-dev libvulkan-dev opencl-headers libglfw3 libglfw3-dev libsdl2-dev freeglut3-dev xorg-dev
   libassimp-dev libfreetype-dev libxmu-dev libsimdjson-dev libgltf-dev
-  glmark2-x11 glmark2-es2-x11
-  libjack-dev libpipewire-0.3-dev
-  rustc rustfmt bindgen
+  libjack-dev
+  rustc rustfmt
   "
+
+if [ $VERSION_ID == "20.04" ]; then
+  # Ubuntu 20.04
+  packages+=" \
+    glmark2 glmark2-es2
+    libpipewire-0.2-dev
+    "
+else
+  # Ubuntu 24.04
+  packages+=" \
+    gnome-shell-extension-manager
+    glmark2-x11 glmark2-es2-x11
+    libpipewire-0.3-dev
+    bindgen
+    "
+fi
+
 sudo apt install $packages
 
 # 需要在源那里enable Source code
